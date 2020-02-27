430d6111588b2553bab42d79d46d3573abff1ca4
lapohjan <59822237+lapohjan@users.noreply.github.com>
2020-02-27 13:22:25 +0200

    fixed script
    Moved picture slider -script outside the document ready -function

--- laura/myscripts.js
+++ laura/myscripts.js
@@ -1,30 +1,4 @@
 $(document).ready(function() {
-  //Picture slider
-
-  var slideIndex = 1;
-  showDivs(slideIndex);
-
-  function plusDivs(n) {
-    showDivs((slideIndex += n));
-  }
-
-  function showDivs(n) {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    if (n > x.length) {
-      slideIndex = 1;
-    }
-    if (n < 1) {
-      slideIndex = x.length;
-    }
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-    }
-    if (x[slideIndex - 1]) {
-      x[slideIndex - 1].style.display = "block";
-    }
-  }
-
   //Back to top -button
 
   //Get the button:
@@ -45,10 +19,36 @@ $(document).ready(function() {
       mybutton.style.display = "none";
     }
   }
-
-  // When the user clicks on the button, scroll to the top of the document
-  function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-  }
 });
+
+//Picture slider
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs((slideIndex += n));
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  if (x[slideIndex - 1]) {
+    x[slideIndex - 1].style.display = "block";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
