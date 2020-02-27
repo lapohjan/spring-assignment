7c1e5997807278bae2c799a38f8b2bfbfee1e7b9
lapohjan <59822237+lapohjan@users.noreply.github.com>
2020-02-27 12:57:36 +0200

    fixed document ready -function
    Added jquery and document ready -function.

--- laura/myscripts.js
+++ laura/myscripts.js
@@ -1,49 +1,54 @@
-//Picture slider
+$(document).ready(function() {
+  //Picture slider
 
-var slideIndex = 1;
-showDivs(slideIndex);
+  var slideIndex = 1;
+  showDivs(slideIndex);
 
-function plusDivs(n) {
-  showDivs((slideIndex += n));
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {
-    slideIndex = 1;
+  function plusDivs(n) {
+    showDivs((slideIndex += n));
   }
-  if (n < 1) {
-    slideIndex = x.length;
+
+  function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    if (n > x.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+      slideIndex = x.length;
+    }
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    if (x[slideIndex - 1]) {
+      x[slideIndex - 1].style.display = "block";
+    }
   }
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+
+  //Back to top -button
+
+  //Get the button:
+  mybutton = document.getElementById("myBtn");
+
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function() {
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
   }
-  if (x[slideIndex - 1]) {
-    x[slideIndex - 1].style.display = "block";
+
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
   }
-}
-
-//Back to top -button
-
-//Get the button:
-mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {
-  scrollFunction();
-};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+});
